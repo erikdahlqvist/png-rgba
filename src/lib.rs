a641bb8ec37afdaa2674007a180272fe31072046
Erik Dahlkvist <erikdahlqvist@protonmail.com>
2026-06-23 16:07:26 +0200

    Added verification of png sign

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::fs;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+const PNG_SIGN: &[u8] = &[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn png(path: &str) -> Result<Vec<Vec<u8>>, String> {
+    let input = match fs::read(path) {
+        Ok(input) => input,
+        Err(_) => return Err(format!("Could not read file at: {path}")),
+    };
+
+    let sign_bytes = &input[..8];
+    if sign_bytes != PNG_SIGN {
+        return Err(String::from("Invalid PNG sign bytes"));
     }
+
+    Ok(vec![]) // Temp return
 }
