fe2c64ce7a48e35e3b09b84411f2c379d5828e9e
Erik Dahlkvist <erikdahlqvist@protonmail.com>
2026-06-27 19:08:38 +0200

    Improved image generation script

--- tests/images/generated/generate_images.py
+++ tests/images/generated/generate_images.py
@@ -3,9 +3,11 @@ import png
 W = 64
 H = 64
 
-def gen_rgba(depth):
+def gen_rgb(depth, alpha):
     rows = []
     max_value = 2**depth - 1
+
+    print_a = "a" if alpha else ""
 
     for y in range(H):
         row = []
@@ -14,48 +16,26 @@ def gen_rgba(depth):
             r = (x * max_value) // (W - 1)
             g = (y * max_value) // (H - 1)
             b = max_value // 2
-            a = ((x + y) * max_value) // (W + H - 2)
-            row.extend([r, g, b, a])
+
+            if alpha:
+                a = ((x + y) * max_value) // (W + H - 2)
+                row.extend([r, g, b, a])
+            else:
+                row.extend([r, g, b])
 
         rows.append(row)
 
-    with open(f"rgba{depth}-{W}x{H}.png", "wb") as f:
+    with open(f"rgb{print_a}{depth}-{W}x{H}.png", "wb") as f:
         writer = png.Writer(
             width=W,
             height=H,
-            alpha=True,
+            alpha=alpha,
             bitdepth=depth,
             greyscale=False,
         )
         writer.write(f, rows)
 
-def gen_rgb(depth):
-    rows = []
-    max_value = 2**depth - 1
-
-    for y in range(H):
-        row = []
-
-        for x in range(W):
-            r = (x * max_value) // (W - 1)
-            g = (y * max_value) // (H - 1)
-            b = max_value // 2
-            row.extend([r, g, b])
-
-        rows.append(row)
-
-    with open(f"rgb{depth}-{W}x{H}.png", "wb") as f:
-        writer = png.Writer(
-            width=W,
-            height=H,
-            alpha=False,
-            bitdepth=depth,
-            greyscale=False,
-        )
-        writer.write(f, rows)
-
-
-gen_rgba(8)
-gen_rgba(16)
-gen_rgb(8)
-gen_rgb(16)
+gen_rgb(8, True)
+gen_rgb(16, True)
+gen_rgb(8, False)
+gen_rgb(16, False)
